50832af7ccea5d151f81d68d89f6743936943189
shemesh7 <yehonatan.shemesh1@live.biu.ac.il>
2025-06-10 23:33:37 +0300

    Finished the assignment *before debugging*

--- Exercise_3/CPU-Scheduler.c
+++ Exercise_3/CPU-Scheduler.c
@@ -11,6 +11,9 @@
 #define MAX_DESCRIPTION 101
 #define MAX_PROCESSORS 1000
 #define MAX_LINE 257
+#define RUNNING_MSG "%d → %d: %s Running %s.\n"
+#define IDLE_MSG "%d → %d: Idle.\n"
+
 #define INTRO \
 "══════════════════════════════════════════════\n" \
 ">> Scheduler Mode : %s\n" \
@@ -59,13 +62,14 @@ typedef struct {
 
 void parseCSV(char* FilePath, Process* P, int * count);
 Process parseProcess(const char* line);
-void sigalarmIgnore();
-int cmpFCFS(Process p1, Process p2);
+void sigalarm_ignore();
+int cmpFCFS(__attribute__((unused)) Process p1, Process p2);
 int cmpLowerPriority(Process p1, Process p2);
 int cmpBurstTime(Process p1, Process p2);
 int cmpArrival(Process p1, Process p2);
 void Enqueue(ReadyQueue* q, Process p);
 Process Dequeue(ReadyQueue* q);
+void EnqueueNewArrivals(ReadyQueue* queue, Process procs[], int* startIndex, int count, struct timespec startingTime);
 int GetTotalTime(struct timespec starting);
 void RunScheduler(Scheduler scheduler, Process procs[], int count);
 void SortProcs (Process procs[], int count, int  (cmpProcs) (Process p1, Process p2));
@@ -129,18 +133,17 @@ void parseCSV(char* FilePath, Process* P, int * count) {
     }
     // parse every process, line by line
     char L[MAX_LINE];
-    while (fscanf(file, "%[^\n]%*c", L) == 1)
-    {
+    while (fscanf(file, "%[^\n]%*c", L) == 1) {
         Process proc = parseProcess(L);
         proc.index = *count;
         P[*count] = proc;
         (*count)++;
     }
-    if (ferror(file))
-    {
+    if (ferror(file)) {
         perror("fscanf error");
         exit(1);
     }
+    fclose(file);
     SortProcs(P, *count, cmpArrival);
 }
 
@@ -156,11 +159,11 @@ Process parseProcess(const char* line) {
     char *save_ptr;  // keep original pointer for free
     save_ptr = dup;
 
-    strcpy(process.name, strtok_r(dup, ",", &dup));
-    strcpy(process.description, strtok_r(dup, ",", &dup));
-    process.arrival_time = atoi(strtok_r(dup, ",", &dup));
-    process.burst_time = atoi(strtok_r(dup, ",", &dup));
-    process.priority = atoi(strtok_r(dup, ",", &dup));
+    strcpy(process.name, strtok_r(dup, ",", &save_ptr));
+    strcpy(process.description, strtok_r(dup, ",", &save_ptr));
+    process.arrival_time = atoi(strtok_r(dup, ",", &save_ptr));
+    process.burst_time = atoi(strtok_r(dup, ",", &save_ptr));
+    process.priority = atoi(strtok_r(dup, ",", &save_ptr));
     // the index will be initialized outside this function
 
     free(save_ptr);
@@ -178,7 +181,7 @@ void RunScheduler(Scheduler scheduler, Process procs[], int count) {
     struct sigaction act;
     act.sa_flags = 0;
     sigemptyset(&act.sa_mask);
-    act.sa_handler = sigalarmIgnore;
+    act.sa_handler = sigalarm_ignore;
     sigaction(SIGALRM, &act, NULL);
     // print intro
     printf(INTRO, scheduler.name);
@@ -190,12 +193,87 @@ void RunScheduler(Scheduler scheduler, Process procs[], int count) {
 
     /**
      * Main loop: enqueue new arrivals and check if the current process is done
-     */
-     int starting = 0;
-     Process currProc;
-     bool isRunning = false;
-     bool isIdle = false;
+    */
+    int starting = 0;
+    Process currProc;
+    bool isRunning = false;
+    bool isIdle = false;
+    struct timespec procStartTime;
+    bool isProcArrived = starting >= count;
+    int turnAround = 0;
+    int totalWaiting = 0;
+    int iter = 0;
+    int idleStart = -1;
 
+    while ((!isProcArrived) || (Q.size > 0) || isRunning) {
+        isProcArrived = starting >= count;
+        if (!isProcArrived) {
+            EnqueueNewArrivals(&Q, procs, &starting, count, startT);
+            isProcArrived = starting >= count;
+        }
+        if (isRunning) {
+            int processUptime = (int)GetTotalTime(procStartTime);
+            int schedulerUptime = (int)GetTotalTime(startT);
+            if (processUptime >= currProc.burst_time) {
+                totalWaiting += schedulerUptime - currProc.burst_time - currProc.arrival_time;
+                isRunning = false;
+
+                printf(RUNNING_MSG, schedulerUptime - currProc.burst_time, schedulerUptime, currProc.name, currProc.description);
+                fflush(stdout);
+
+                if (isProcArrived && (Q.size == 0)) {
+                    turnAround = (int)GetTotalTime(startT);
+                    break;
+                }
+            }
+            else if (scheduler.timeQuantum != -1) {
+                if (processUptime >= scheduler.timeQuantum) {
+                    totalWaiting += schedulerUptime - scheduler.timeQuantum - currProc.arrival_time;
+                    isRunning = false;
+
+                    printf(RUNNING_MSG, schedulerUptime - scheduler.timeQuantum, schedulerUptime, scheduler.name, currProc.description);
+                    fflush(stdout);
+
+                    Process modifiedProcess = currProc;
+                    modifiedProcess.arrival_time = schedulerUptime;
+                    modifiedProcess.burst_time -= scheduler.timeQuantum;
+                    Enqueue(&Q, modifiedProcess);
+                }
+            }
+        }
+
+
+        if (!isRunning && (Q.size > 0)) {
+            if (isIdle) {
+                int schedulerUptime = (int)GetTotalTime(startT);
+                printf(IDLE_MSG, idleStart, schedulerUptime);
+                fflush(stdout);
+                isIdle = false;
+                idleStart = -1;
+            }
+            isRunning = true;
+            clock_gettime(CLOCK_MONOTONIC, &procStartTime);
+            currProc = Dequeue(&Q);
+        }
+
+        if (!isRunning && (Q.size == 0) && (!isProcArrived) && !isIdle) {
+            isIdle = true;
+            idleStart = (int)GetTotalTime(startT);
+        }
+        iter++;
+        ualarm((int)1e5, 0);
+        pause();
+    }
+
+    // print outro
+    if (scheduler.CalculateTurnAround) {
+        printf(OUTRO_TA, turnAround); // add the parameters
+        fflush(stdout);
+    }
+    else {
+        printf(OUTRO_TW, (double) totalWaiting / count); // add the parameters
+        fflush(stdout);
+    }
 }
 
 int GetTotalTime(struct timespec starting) {
@@ -234,8 +312,8 @@ void EnqueueNewArrivals(ReadyQueue* queue, Process procs[], int* startIndex, int
 }
 
 void SortProcs (Process procs[], int count, int  (cmpProcs) (Process p1, Process p2)) {
-    for (int i = 0; i < count; i++) {
-        for (int j = 0; j < count; j++) {
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = 0; j < count - 1; j++) {
             if (cmpProcs(procs[j], procs[j+1]) > 0) { // swap
                 Process temp = procs[j];
                 procs[j] = procs[j+1];
@@ -245,16 +323,23 @@ void SortProcs (Process procs[], int count, int  (cmpProcs) (Process p1, Process
     }
 }
 
+void sigalarm_ignore() {}
 
-int cmpFCFS(Process p1, Process p2) {
+int cmpFCFS(__attribute__((unused)) Process p1, __attribute__((unused)) Process p2) {
     return 0;
 }
 int cmpLowerPriority(Process p1, Process p2) {
-    return p1.priority - p2.priority;
+    if (p1.priority != p2.priority)
+        return p1.priority - p2.priority;
+    return cmpArrival(p1,p2);
 }
 int cmpBurstTime(Process p1, Process p2) {
-    return p1.burst_time - p2.burst_time;
+    if (p1.burst_time != p2.burst_time)
+        return p1.burst_time - p2.burst_time;
+    return cmpArrival(p1,p2);
 }
 int cmpArrival(Process p1, Process p2) {
-    return p1.arrival_time - p2.arrival_time;
+    if (p1.arrival_time != p2.arrival_time)
+        return p1.arrival_time - p2.arrival_time;
+    return p1.index - p2.index;
 }
